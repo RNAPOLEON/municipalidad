93094a1431ddda9b03d77c58b4164c82334fbec2
RNAPOLEÓN <romanonapoloensr@gmail.com>
2024-10-07 11:30:29 -0300

    ver-pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import { MainComponent } from "./commponets/main/main.component";
 import { FixedComponent } from "./commponets/fixed/fixed.component"; 
 import { GridOneComponent } from "./commponets/grid-one/grid-one.component";
 import { ContentComponent } from "./commponets/content/content.component";
+import { FixedVideoComponent } from './commponets/fixed-video/fixed-video.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
@@ -17,6 +18,7 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
     FixedComponent,
     GridOneComponent,
     ContentComponent,
+    FixedVideoComponent,
     FontAwesomeModule 
   ],
   templateUrl: './app.component.html',
--- src/app/commponets/fixed-video/fixed-video.component.ts
+++ src/app/commponets/fixed-video/fixed-video.component.ts
@@ -5,8 +5,34 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './fixed-video.component.html',
-  styleUrl: './fixed-video.component.scss'
+  styleUrls: ['./fixed-video.component.scss']
 })
-export class FixedVideoComponent {
 
+export class FixedVideoComponent {
+  /*
+  constructor(){
+    const fixedVideo = document.getElementById('fixedVideo');
+    const delete = document.getElementById('delete');
+    delete?.addEventListener("click", () =>{
+      fixedVideo.classList.add('displayNone');
+      console.log("ya casi ");
+    });
+  
+  }
+    */
 }
+
+/*
+
+<div id="fixed-video" class="expand">
+    <dir class="delet">
+        <div class="expand-menu" id="expandClick">
+    
+            <div class="line"></div>
+            <div class="line"></div>
+            <div class="line"></div>
+        </div>
+    </dir>
+    <div class="content-video"></div>
+</div>
+ */
